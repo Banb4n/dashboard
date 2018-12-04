4a9a6f0e4e3d3f181a7c420fcfc7398eefd02dc3
Banb4n <alban.such@gmail.com>
2018-12-04 23:40:40 +0100

    Add homepage

--- src/components/pages/website/HomePage.js
+++ src/components/pages/website/HomePage.js
@@ -1,12 +1,22 @@
 /* @flow */
 import * as React from 'react';
-import { StyleSheet } from 'aphrodite-jss';
+import { StyleSheet, css } from 'aphrodite-jss';
+import homePattern from '../../../assets/imgs/home.svg';
 
-const STYLES = StyleSheet.create({});
+const STYLES = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100vh',
+        backgroundImage: `url(${homePattern})`,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 function HomePage(appProps: {}): React.Node {
     return (
-        <div className="HomePage">
+        <div className={css(STYLES.container)}>
             <h1>HomePage</h1>
         </div>
     );
